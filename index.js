65646262ae43f5bcd04f9a64ff0e9f22ccf03d7f
Yuki Okamoto <yuuki_2001_0206@icloud.com>
2022-07-12 22:31:28 +0900

    add: logic to connect mongodb

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require('./.config/config');
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose
+  .connect(mongoURL) //able to IP address, but mongo(which is from docker-compose.yml services name) is better cuz you dont have to change every single time you run.
+  .then(() => console.log('successfully connected to DB'))
+  .catch((e) => console.log(e));
+
 app.get('/', (req, res) => {
-  res.send('<h2>Hello!!!<lsh2>');
+  res.send('<h2>Hello World!!!<lsh2>');
 });
 
 const port = process.env.PORT || 3000;
