746ec57070a28e275126d271361d4e1a84d66151
Yuki Okamoto <yokamoto@csumb.edu>
2022-07-17 00:47:45 +0900

    add: post api

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const {
   MONGO_PORT,
 } = require('./.config/config');
 
+const postRouter = require('./routes/postRoutes');
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -23,10 +25,14 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('<h2>Hello World!!!<lsh2>');
 });
 
+// localhost:3000/api/v1/post
+app.use('/api/v1/posts', postRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
